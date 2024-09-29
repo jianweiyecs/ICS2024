ab6a9e40cc10735dbab9e93662f5eaeba32eefcb
tracer-ics2024 <tracer@njuics.org>
2024-09-29 16:49:44 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:49:44 up 7 days,  6:36,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -89,8 +89,8 @@ static int cmd_x(char* args){
   sscanf(args,"%d %x",&n,&address);
   int i;
   for(i = 0;i < n;i++){
-    printf("address: %x  memory: %x\n",address,paddr_read(address,4));
-    address = address + 32;
+    printf("address: %x  memory: %8x\n",address,paddr_read(address,4));
+    address = address + 4;
   }
   return 0;
 }
