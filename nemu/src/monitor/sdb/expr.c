4c3b68efbf40c544e4444bd40248b080bc6781a8
tracer-ics2024 <tracer@njuics.org>
2024-10-11 17:50:49 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  17:50:49 up  7:34,  0 users,  load average: 0.04, 0.01, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -79,6 +79,7 @@ static Token tokens[32] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
+  printf("Inter make_token\n");
   int position = 0;
   int i;
   regmatch_t pmatch;
