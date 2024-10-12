06680e3ff1c93bc26247e30a0724eac6bc9db7a7
tracer-ics2024 <tracer@njuics.org>
2024-10-12 15:30:51 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  15:30:51 up 1 day,  5:14,  0 users,  load average: 0.08, 0.05, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -37,14 +37,11 @@ static struct rule {
    * Pay attention to the precedence level of different rules.
    */
 
-  // {"\\d+", TK_NUM}, //myInt
-  {"[0-9]*", TK_NUM},
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  // {"==", TK_EQ},        // equal
 
-  
+  {"[0-9]*", TK_NUM},
   {"\\-", '-'}, //sub
   {"\\*", '*'}, //mul
   {"\\/", '/'}, //div
