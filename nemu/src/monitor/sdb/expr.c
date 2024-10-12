8cc18118dd8c7e0a66950304d0b392c14b3db249
tracer-ics2024 <tracer@njuics.org>
2024-10-12 10:48:18 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  10:48:18 up 1 day, 31 min,  0 users,  load average: 0.02, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -41,7 +41,7 @@ static struct rule {
   {"\\+", '+'},         // plus
   {"==", TK_EQ},        // equal
 
-  {"\\d", TK_NUM}, //Int
+  {"\\b\\d+\\b", TK_NUM}, //Int
   {"\\-",'-'}, //sub
   {"\\*",'*'}, //mul
   {"\\/",'/'}, //div
