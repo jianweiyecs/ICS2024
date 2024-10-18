8dd149193c4e1f56ea8251585bbae99f62c63e63
tracer-ics2024 <tracer@njuics.org>
2024-10-18 16:56:05 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  16:56:05 up 7 days,  6:39,  0 users,  load average: 0.06, 0.14, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -298,7 +298,7 @@ static u_int32_t eval(int l,int r){
     return 0;
   }else if(l == r){
     // printf("tokens[l].str is %s change is %u", tokens[l].str, (u_int32_t)strtol(tokens[l].str, NULL, 16));
-    return strtol(tokens[l].str, NULL, 16);
+    return (u_int32_t)strtol(tokens[l].str, NULL, 16);
   }else if(check_parentheses(l, r)){
     return eval(l + 1, r - 1);
   }else{
