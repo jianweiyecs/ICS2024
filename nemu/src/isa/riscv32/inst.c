0d894fb6b0170374cfc059c73126cae91091f2c1
tracer-ics2024 <tracer@njuics.org>
2024-11-01 14:38:52 +0800

    >  compile NEMU 2410103009 叶建伟 Linux yjw 5.10.16.3-microsoft-standard-WSL2 #1 SMP Fri Apr 2 22:23:49 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux  14:38:52 up 21 days,  4:22,  0 users,  load average: 0.04, 0.06, 0.07

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -38,7 +38,7 @@ enum {
 #define immJ() do { *imm = SEXT((BITS(i,31,31) << 19 \
 | BITS(i,19,12) << 11 | BITS(i,20,20) << 10 | BITS(i,30,21)) << 1, 20);} while(0) //may 21?(I don't think so)
 
-#define immB() do { *imm = SEXT((BITS(i,31,31) << 11 | BITS(i,7,7) << 10 | BITS(i,30,25) << 4 | BITS(i,11,8)) << 1, 13);} while(0)
+#define immB() do { *imm = SEXT((BITS(i,31,31) << 11 | BITS(i,7,7) << 10 | BITS(i,30,25) << 4 | BITS(i,11,8)) << 1, 12);} while(0)
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
   uint32_t i = s->isa.inst.val;
   int rs1 = BITS(i, 19, 15);
